52b4ae17efcfdb418b72575e47e5942607b0b778
Pabl0Parra <pabsblue16@gmail.com>
2022-07-05 10:13:23 +0200

    initial first test failing

---
+++ src/__test__/main-page.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+
+import { MainPage } from "./components/main-page";
+
+// --- Test Driven Dev -----
+// 1. RED (failed) --> create a test that fails
+// 2. GREEN (passed) --> write minimun code in order to pass the test
+// 3. Refactor --> apply clean code & other good practices (like code smells..)
+
+describe("Main Page mount", () => {
+  it("must display the main page title", () => {
+    render(<MainPage />);
+    expect(screen.getByText(/simpsons quotes/i)).toBeInTheDocument();
+  });
+});
