6145bfa78d3be8fab8126645660d3ef814c2641d
Pabl0Parra <pabsblue16@gmail.com>
2022-07-05 10:26:09 +0200

    second run for the RED part

--- src/__test__/main-page.test.js
+++ src/__test__/main-page.test.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 
-import { MainPage } from "./components/main-page";
+import { MainPage } from "../components/main-page";
 
 // --- Test Driven Dev -----
 // 1. RED (failed) --> create a test that fails
 // 2. GREEN (passed) --> write minimun code in order to pass the test
 // 3. Refactor --> apply clean code & other good practices (like code smells..)
 
+// This will fail since the component is not being created yet (MainPage)
 describe("Main Page mount", () => {
   it("must display the main page title", () => {
     render(<MainPage />);
